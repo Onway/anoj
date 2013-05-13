446581cb96af4db830c9011d2744d14b892bc7da
Liu Yi <aluohuai@126.com>
2013-05-13 16:08:24 +0800

    finished solution.py, rm analyse

--- devtools/solution.py
+++ devtools/solution.py
@@ -4,6 +4,7 @@
 # Author        : Yiu Yi <aluohuai@126.com>
 # Create Date   : 2013-05-04
 
+import os
 import sys
 import time
 import json
@@ -26,10 +27,68 @@ def request_judge(xmlfile):
         print 'sending request >>>', repr(kdict)
         print
         urllib2.urlopen(urllib2.Request(URL, urllib.urlencode(kdict)))
-        # time.sleep(2)
+        time.sleep(2)
+
+def print_html_head():
+    print '''
+<!DOCTYPE html
+PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+    <title> autotest result </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <style type="text/css">
+        table {
+            width: 70%;
+            text-align: center;
+        }
+    </style>
+    </head>
+    <body>
+        <table align="center">
+            <tr>
+                <th>run_id</th>
+                <th>problem_id</th>
+                <th>language</th>
+                <th width="30%">description</th>
+                <th>expect</th>
+                <th>result</th>
+            </tr>
+'''
+
+def print_html_foot():
+    print '''
+        </table>
+    </body>
+    <script type="text/javascript">
+        var trs = document.getElementsByTagName("tr");
+        for (i = 1; i < trs.length; i += 2) {
+            trs[i].setAttribute("bgcolor", "#f0f0f0");
+        }
+    </script>
+</html>
+'''
+
+def get_real_result(respath, rid):
+    fpath = os.path.join(respath, rid)
+    fd = open(fpath, "r")
+    result = json.load(fd)
+    return result["result"]
 
 def analyse_result(xmlfile, respath):
-    pass
+    tags = ["rid", "pid", "lang", "desc", "expect"]
+
+    print_html_head()
+    root = ET.parse(xmlfile).getroot()
+    for solution in root:
+        print "<tr>"
+        for tag in tags:
+            print "<td>" + solution.find(tag).text + "</td>"
+        print "<td>" + get_real_result(respath, solution.find("rid").text)\
+                + "</td>"
+        print "</tr>"
+    print_html_foot()
 
 def print_usage(argv):
     print 'Usage: %s <xmlfile> [respath]' % argv[0]
